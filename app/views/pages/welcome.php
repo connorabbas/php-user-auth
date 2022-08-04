0a507e19c20ea04cd124febb0c64bff51e8f20c1
connorabbas <connora@jdsindustries.com>
2022-08-03 20:25:24 -0500

    small change, done for today

--- app/views/pages/welcome.php
+++ app/views/pages/welcome.php
@@ -7,10 +7,6 @@ $this->layout('template::main', [
 <div class="d-flex align-items-center justify-content-center" style="height: 90vh">
     <div class="text-center">
         <h3 class="mb-3">PHP User Auth Application</h3>
-        <p class="mb-3 text-muted">Built With PHP Mini Framework</p>
-        <p class="mb-0 small">
-            <a href="https://github.com/connorabbas/php-mini-framework#php-mini-framework" target="_blank">Documentation</a>
-        </p>
-        <br>
+        <p class="mb-3 text-muted">Built With <a href="https://github.com/connorabbas/php-mini-framework#php-mini-framework" target="_blank">PHP Mini Framework</a></p>
     </div>
 </div>
