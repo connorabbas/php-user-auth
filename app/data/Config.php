99338ba7e6d2152e90f4b41204a7b7a5838e7889
connorabbas <abbasconnor@gmail.com>
2023-01-08 10:26:48 -0600

    framework updates

--- app/data/Config.php
+++ app/data/Config.php
@@ -30,19 +30,6 @@ class Config
                         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
                     ]
                 ],
-                'alt' => [
-                    'driver' => $env['DB_DRIVER'],
-                    'host' => $env['DB_HOST'],
-                    'username' => $env['DB_USERNAME'],
-                    'password' => $env['DB_PASSWORD'],
-                    'name' => $env['DB_NAME_2'],
-                    'pdo_options' => [
-                        PDO::ATTR_PERSISTENT => false,
-                        PDO::ATTR_EMULATE_PREPARES => false,
-                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-                    ]
-                ],
             ],
             'plates_templates' => [
                 'folders' => [
